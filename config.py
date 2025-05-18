2448cc490ef6da8fd807c994abe5843e21575b29
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2025-05-18 21:48:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 28356436))
-API_HASH = getenv("API_HASH", "90b2e5a5868e07ff9162f9d4380ac5b2")
-OWNER_ID = int(getenv("OWNER_ID", "7160246535"))
+API_ID = int(getenv("API_ID", 28822380))
+API_HASH = getenv("API_HASH", "242ffb70cbc4bebaeed1809ee4d72d0e")
+OWNER_ID = int(getenv("OWNER_ID", "7710642242"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "knight_society")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "itsRareBeautySelenaGomez")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "AnimalJanwarrrr")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/+atOIt_3H7QVlMjA1")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "tabahi_tabahi")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "smartness_to_hai")
 
 # Random Start Images
 IMG = [
