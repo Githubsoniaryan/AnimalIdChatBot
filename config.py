e37e4299a06972246aff6c97197b98806daed410
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2025-05-19 10:46:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = getenv("API_HASH", "242ffb70cbc4bebaeed1809ee4d72d0e")
 OWNER_ID = int(getenv("OWNER_ID", "7710642242"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/+atOIt_3H7QVlMjA1")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/+HsTv37kyfMVhODll")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "tabahi_tabahi")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "smartness_to_hai")
 
